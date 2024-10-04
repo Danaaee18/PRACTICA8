9f7af8247e66964ba3cb4c6dbe3cd7793b20a78c
Danaaee18 <danaesuarezpineda@gmail.com>
2024-10-04 14:10:45 -0600

    Update TABLAS DE MULTIPLICAR.C

--- TABLAS DE MULTIPLICAR.C
+++ TABLAS DE MULTIPLICAR.C
@@ -3,8 +3,16 @@ Laboratorio práctica 8
 Tablas de multiplicar*/
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int i;
+int P;
+int n;
 int main ()
 {
+  printf("Dame un número 
+
 for (i=0;i<=10; i++)
 
 
