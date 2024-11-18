eab3adebfe3163c8189e0d9e61623c88bd539b0c
Danaaee18 <danaesuarezpineda@gmail.com>
2024-11-18 12:41:22 -0600

    Update and rename TABLAS DE MULTIPLICAR.C to TABLAS DE MULTIPLICAR.c

---
+++ TABLAS DE MULTIPLICAR.c
@@ -0,0 +1,25 @@
+/*Danae Suarez Pineda
+Laboratorio práctica 8
+Tablas de multiplicar*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int i;
+int P;
+int n;
+int main ()
+{
+    printf("Dame un numero: ");
+    scanf("%d", &n);
+    
+    for (i = 1; i <= 10; i++)
+    {
+        P = n * i;
+        printf("%d\n",P);
+    }
+    
+    return 0;
+}
+
